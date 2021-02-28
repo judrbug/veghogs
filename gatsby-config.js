0c5dd7140ff6f337363cc3ddfd33685940e54668
judrbug <meffordhannah@gmail.com>
2021-02-28 12:48:16 -0800

    added deploy script and typography packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,13 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
